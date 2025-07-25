e30a7ae43fe9e61dcb8fa09570ce6ecbc03be681
Mirik-ua <stochkomira@gmail.com>
2025-07-26 00:03:00 +0200

    add header types

--- src/components/header/ToggleBtn.tsx
+++ src/components/header/ToggleBtn.tsx
@@ -1,8 +1,10 @@
-export const ToggleBtn = ({
-  handleClick,
-  text,
-  classname,
-}: any & { classname?: string }) => (
+type ToggleType = {
+  handleClick: () => void
+  text: string
+  classname: string
+}
+
+export const ToggleBtn = ({ handleClick, text, classname }: ToggleType) => (
   <button
     onClick={handleClick}
     className={`px-3 py-1 rounded-md border transition duration-500 ${classname}`}
