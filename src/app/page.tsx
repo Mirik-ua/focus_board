efa959798baff336f38f05bd577018c99c9f23e9
Mirik-ua <stochkomira@gmail.com>
2025-07-26 00:54:22 +0200

    add styled ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { TodoWrapper } from '@/components/todo/index'
 import { FilterTypes, Todo, TodoField } from '@/types/todo'
 import { useCallback, useState } from 'react'
+import { SectionWrap } from '@/components/todo/SectionWrap'
 
 const initialVal = (label: string, id: number) => ({
   id: id.toString(),
@@ -55,24 +56,15 @@ export default function TodoList() {
   }, [])
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <input
-          className="border-2"
-          type="text"
-          value={labelField.value}
-          onChange={handleField}
-        />
-        {labelField.error ? <p>{labelField.error}</p> : null}
-        <button className="bg-black text-white" onClick={handleClick}>
-          btn
-        </button>
-        <TodoWrapper
-          arr={arr}
-          filter={filter}
-          handleChangeRadio={handleChangeRadio}
-          handleChange={handleChange}
-        />
+    <div
+      className="flex font-sans  pt-20  px-6 gap-16  pb-6"
+      style={{
+        height: 'calc(100vh - 100px)',
+        maxHeight: 'calc(100vh - 100px)',
+      }}
+    >
+      <main className="flex overflow-x-scroll gap-[32px] ">
+        <SectionWrap />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
