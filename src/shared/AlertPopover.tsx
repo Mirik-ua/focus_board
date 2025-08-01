7e7d52d90b8669fb6539fbd0c583409e9a4a09e8
Mirik-ua <stochkomira@gmail.com>
2025-08-01 13:09:24 +0200

    fix cancel button color accent

--- src/shared/AlertPopover.tsx
+++ src/shared/AlertPopover.tsx
@@ -34,7 +34,7 @@ export function AlertPopover({ sectionName, onDelete }: Props) {
         </h3>
         <div className="border-t-2 pt-4 flex items-center justify-between border-white">
           <Button
-            className="px-4 py-2 rounded-lg border border-white/10 bg-white/5 text-white hover:bg-white/10 transition"
+            className="px-4 py-2 rounded-lg border border-white/10 dark:bg-white/5 bg-black/30 text-white dark:hover:bg-white/10 hover:bg-black/50 transition"
             onClick={handleOpenChange}
           >
             Cancel
